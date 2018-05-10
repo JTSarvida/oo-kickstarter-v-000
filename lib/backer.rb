c36e1197fcf929d9ded9f893428b91ab4b3bb621
Joshua Tan <jtsarvida@gmail.com>
2018-05-10 05:20:22 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -2,7 +2,15 @@ class Backer
   
   attr_accessor :name
   
+  @backed_projects = []
+  
   def initialize(name)
     @name = name
   end
+  
+  def back_project(project)
+    @backed_projects << project
+  end
+  
+  
 end
